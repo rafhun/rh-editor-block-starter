87fe8ba4758c87873ae8ac73a6d7042aef405549
Raphael Hüni <hueni@werbelinie.ch>
2020-12-24 15:29:15 +0100

    fix use of useBlockProps and declare support for API v2

--- src/EditorBlock/save.js
+++ src/EditorBlock/save.js
@@ -1,9 +1,14 @@
 /** External dependencies */
 
 /** WordPress dependencies */
+import { useBlockProps } from "@wordpress/block-editor"
 
 /** Internal dependencies */
 
 export default function save({ attributes }) {
-  return null
+  return (
+    <div {...useBlockProps.save()}>
+      <p>Save Contents</p>
+    </div>
+  )
 }
